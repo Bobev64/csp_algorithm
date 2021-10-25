2d5914b63088990ea598b79839998cfed74c3807
Fritz Geib <fritz@standland.org>
2021-10-25 14:39:01 -0400

    Commit message

--- cubes.py
+++ cubes.py
@@ -1,28 +1,28 @@
 import turtle
 
-def rectPrism(painter: turtle.Turtle, l: float, w: float, h: float, colors: list) -> None:
-    painter.color(colors[0])
-    painter.begin_fill()
-    painter.setheading(0)
+def rectPrism(t: turtle.Turtle, l: float, w: float, h: float, colors: list) -> None:
+    t.color(colors[0])
+    t.begin_fill()
+    t.setheading(0)
     for i in range(5):
-        painter.forward(w if i%2 == 0 else h)
-        painter.left(90)
-        if i == 3: painter.end_fill()
+        t.forward(w if i%2 == 0 else h)
+        t.left(90)
+        if i == 3: t.end_fill()
 
-    painter.color(colors[1])
-    painter.begin_fill()
+    t.color(colors[1])
+    t.begin_fill()
     for i in range(5):
-        painter.forward(h if i%2 == 0 else l/2)
-        painter.right(45 if i%2 == 0 else 135)
-        if i == 3: painter.end_fill()
+        t.forward(h if i%2 == 0 else l/2)
+        t.right(45 if i%2 == 0 else 135)
+        if i == 3: t.end_fill()
 
-    painter.color(colors[2])
-    painter.begin_fill()
+    t.color(colors[2])
+    t.begin_fill()
     for i in range(3):
-        painter.forward(l/2 if i%2 == 0 else w)
-        painter.right(-135 if i%2 == 0 else -45)
-    painter.end_fill()
-    painter.penup()
+        t.forward(l/2 if i%2 == 0 else w)
+        t.right(-135 if i%2 == 0 else -45)
+    t.end_fill()
+    t.penup()
 
-def cube(painter: turtle.Turtle, l: float, colors: list) -> None:
-    rectPrism(painter, l, l, l, colors)
+def cube(t: turtle.Turtle, l: float, colors: list) -> None:
+    rectPrism(t, l, l, l, colors)
