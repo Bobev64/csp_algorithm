a3bfacfdad70922377c56f0569d870137d521eb8
Bobev <68569762+Bobev64@users.noreply.github.com>
2021-10-28 14:20:19 -0400

    added a few comments, cleaned up code and repo, turtle_bounce.py still todo

--- bounce.py
+++ bounce.py
@@ -2,16 +2,13 @@ import turtle as trtl
 import random
 import math
 
-# Screen width first tuple, second height
+'''
+This program is meant to take the turtle object and bounce it
+around the boundaries of the screen when it hits a boundary
+it also is meant to change color each time it hits a given border
+and only increment a certain amount of times.
+'''
 
-"""
-drawer = trtl.Turtle()
-drawer.setheading(135)
-drawer.forward(20)
-def bottom_draw(window_width):
-
-    numOfLines = window_width / 12
-"""    
 trtl.shape('turtle')
 
 def turtle_bounce(times):
@@ -23,28 +20,30 @@ def turtle_bounce(times):
     MAX_Y = screenSize[0]
     print(MAX_X, MAX_Y)
 
-#    infinite = float('inf')
+    # Initializing colors to pass to the turtle object, meant to change upon each bounce
     turtle_colors = ["red", "green", "blue"]
     bouncyTurtle.shape('turtle')
-    for color in turtle_colors:
+    for color in turtle_colors: # Should change color per bounce
         bouncyTurtle.color(color)
-        #while incrementer < times:
-            # bouncyTurtle.setheading(random.randint(90, 360))
+        # bouncyTurtle.setheading(random.randint(90, 360))
         
+        # While loop should execute number of times you want turtle to bounce
         while incrementer < times:
         
             x_bouncyTurtle = bouncyTurtle.xcor()
             y_bouncyTurtle = bouncyTurtle.ycor()
             print(x_bouncyTurtle,y_bouncyTurtle)
-            if abs(x_bouncyTurtle) >= MAX_X: # Use abs() to capture both walls
+            # TODO: Make sure turtle heading is set AWAY from the current boundary, otherwise it can get stuck
+            if abs(x_bouncyTurtle) >= MAX_X: 
                 bouncyTurtle.setheading(random.randint(90, 360))
                 
             if abs(y_bouncyTurtle) >= MAX_Y:
                 bouncyTurtle.setheading(random.randint(90, 360))
-            bouncyTurtle.fd(1)
+            bouncyTurtle.fd(1) # Moves turtle forwards 1
 
-turtle_bounce(1)
+turtle_bounce(2) # Should only bounce turtle twice
              
 
 winLoop = trtl.Screen()
+
 winLoop.mainloop()
